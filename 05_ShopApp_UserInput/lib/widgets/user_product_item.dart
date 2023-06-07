023773c5bbec1420ec514633c7eaee5ff5f20aa4
SaiEainkKham <saieainkkhan@gmail.com>
2023-06-08 02:05:42 +0630

    add delete product to shop_app

--- 05_ShopApp_UserInput/lib/widgets/user_product_item.dart
+++ 05_ShopApp_UserInput/lib/widgets/user_product_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:shop_app/screens/edit_product_screen.dart';
+import 'package:shop_app/providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -34,7 +36,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).colorScheme.primary,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,
             ),
