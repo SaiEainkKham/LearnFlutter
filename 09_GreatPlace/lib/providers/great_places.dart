05f19d1b2775cd30365b89e90f1f76ae27cfdbd9
SaiEainkKham <saieainkkhan@gmail.com>
2023-07-30 11:40:25 +0630

    Initialize database to great_places

--- 09_GreatPlace/lib/providers/great_places.dart
+++ 09_GreatPlace/lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:great_place/helpers/db_helper.dart';
 
 import 'package:great_place/models/place.dart';
 
@@ -16,11 +17,15 @@ class GreatPlaces with ChangeNotifier {
     final newPlace = Place(
       id: DateTime.now().toString(),
       title: pickedTitle,
-      location:
-          PlaceLocation(latitude: 0.2, longitude: 0.2, address: 'Somethings'),
+      location: PlaceLocation(latitude: 0.2, longitude: 0.2, address: 'asdf'),
       image: pickedImage,
     );
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
   }
 }
